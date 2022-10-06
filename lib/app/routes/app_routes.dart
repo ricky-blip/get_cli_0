0050a59dc6dc0cb725c572793565a3f2bde7fa83
ricky-blip <ricky.rinaldy77@gmail.com>
2022-10-06 20:31:41 +0700

    update

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -4,9 +4,11 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
+  static const PRODUCTS = _Paths.PRODUCTS;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
+  static const PRODUCTS = '/products';
 }
