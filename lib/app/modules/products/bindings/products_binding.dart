bfd296c96f9782bf8dc1a587d7b7aa383e7dad76
ricky-blip <ricky.rinaldy77@gmail.com>
2022-10-28 14:31:33 +0700

    update

--- lib/app/modules/products/bindings/products_binding.dart
+++ lib/app/modules/products/bindings/products_binding.dart
@@ -1,10 +1,15 @@
 import 'package:get/get.dart';
 
+import 'package:test_getcli/app/modules/products/controllers/test_controller.dart';
+
 import '../controllers/products_controller.dart';
 
 class ProductsBinding extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<TestController>(
+      () => TestController(),
+    );
     Get.lazyPut<ProductsController>(
       () => ProductsController(),
     );
